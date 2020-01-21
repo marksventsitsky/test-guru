31df4aba7e0dc29a635eb46c88daf4854691a035
mark sventsitsky <markengelvd@gmail.com>
2020-01-21 23:43:14 +0300

    minor fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def guthub_url(author, repo)
-    link_to repo, 'https://github.com/' + author + '/' + repo
+    link_to repo, 'https://github.com/' + author + '/' + repo, target: :_blank
   end
 
   def year_now
